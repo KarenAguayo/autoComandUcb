ba7f0603ce729fb7340a33f46477417dac389110
younglee <young.lee@ucb.edu.bo>
2023-04-07 20:43:24 -0400

    PRUEBA GIRAR IZQUIERDA VARIAS VECES

--- src/Auto.test.js
+++ src/Auto.test.js
@@ -1,9 +1,14 @@
 import Auto from "./Auto.js";
 
 describe("Jugar", () => {
-    it("Al enviar I deberia girar una sola vez", () => {
+    it("Al enviar I deberia girar una sola vez la izquierda", () => {
         const auto = new Auto();
         auto.girar("I");
         expect(auto.getPosicionFinal()).toEqual("0,0O");
+    });
+    it("Al enviar IIIII deberia girar seis veces a la izquierda", () => {
+        const auto = new Auto();
+        auto.girar("IIIIII");
+        expect(auto.getPosicionFinal()).toEqual("0,0S");
     });
   });
